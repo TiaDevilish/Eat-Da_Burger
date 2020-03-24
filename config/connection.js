ac914761c67676266e83f3b45849622c214526d0
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-24 14:22:30 -0400

    ops forgot dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
+var dotenv = require("dotenv").config();
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
-  user: "root",
-  password: "Netisedavam7",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: "burgers_db"
 });
 
