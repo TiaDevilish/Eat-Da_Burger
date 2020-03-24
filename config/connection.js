e9aa370822b747d8e373be082f06cdfc6b4f6fce
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-24 17:28:02 -0400

    added heroku code

--- config/connection.js
+++ config/connection.js
@@ -15,14 +15,6 @@ if(process.env.JAWSDB_URL){
   })
 }
 
-// var connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: 3306,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: "burgers_db"
-// });
-
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
