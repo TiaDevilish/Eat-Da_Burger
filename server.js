d67a1956923bbc2c47b9726b3b8efd4673cd83c0
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-24 13:04:54 -0400

    css now working

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(methodOverride("_method"));
